1a7927ea92a769c20b2bbd5bdbc5fbc247f6c8b7
Clement S <c.simonin@lyon.ort.asso.fr>
2018-02-18 22:09:06 +0100

    Static v1

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var app = angular.module("pageModule", ['ngMaterial']);
 
 app.controller("pageController", function($scope){
-  $scope.test = "test";
+
+  $scope.init = function(){
+    $scope.catchPhrase = "E";
+
+    VanillaTilt.init(document.querySelector(".mainCard"), {
+  		max: 20,
+  		speed: 400,
+      scale: 1.1,
+      glare : true,
+      "max-glare": 0.8
+  	});
+  };
 
 });
