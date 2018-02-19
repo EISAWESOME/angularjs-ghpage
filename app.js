ee7de09580d160eafda226e9d83447d17ce4cb4a
Clement S <c.simonin@lyon.ort.asso.fr>
2018-02-19 01:11:26 +0100

    ugly easter egg v1

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-var app = angular.module("pageModule", ['ngMaterial']);
+var app = angular.module("pageModule", ['ngMaterial', 'ngAnimate']);
 
-app.controller("pageController", function($scope){
+app.controller("pageController", function($rootScope, $scope, $timeout){
+
+  var username = "EISAWESOME";
+
 
   $scope.init = function(){
-    $scope.catchPhrase = "E";
+    $scope.catchPhrase = "<clement-s/>";
+    $scope.avatarUrl = "https://github.com/EISAWESOME.png";
 
-    VanillaTilt.init(document.querySelector(".mainCard"), {
-  		max: 20,
-  		speed: 400,
-      scale: 1.1,
-      glare : true,
-      "max-glare": 0.8
-  	});
+
+
+
   };
+
+  var tetris = new Konami(function(){
+    $scope.cardHide = true;
+    //Do the unlock animation
+    $timeout(function(){
+      $scope.tetrisShow = true;
+
+      $timeout(function(){
+        $('.game').blockrain({theme: "candy"});
+      },1001)
+
+
+    }, 1001);
+    $scope.$apply()
+
+
+    //Hide maincard class
+
+    //Create the button to go back
+
+    //Create tetris div
+
+
+  })
 
 });
