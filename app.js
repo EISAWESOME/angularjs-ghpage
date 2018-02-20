cc062cf79a03467b7a1a21d122a2742e2a5b1b10
Clement S <c.simonin@lyon.ort.asso.fr>
2018-02-20 23:04:46 +0100

    Background change + no tilt + fixes

--- app.js
+++ app.js
@@ -1,48 +1,38 @@
-var app = angular.module("pageModule", ['ngMaterial', 'ngAnimate']);
+(function(){
+  var app = angular.module("pageModule", ['ngMaterial', 'ngAnimate']);
 
-app.controller("pageController", function($rootScope, $scope, $timeout){
+  app.controller("pageController", function($rootScope, $scope, $timeout){
 
-  var username = "EISAWESOME";
-
-
-  $scope.init = function(){
-    $scope.catchPhrase = "<clement-s/>";
-    $scope.avatarUrl = "https://github.com/EISAWESOME.png";
-
-    VanillaTilt.init(document.querySelector(".mainCard"), {
-      max: 0,
-      glare: true,
-      "max-glare": 0.4,
-      scale: 1.05,
-    });
+    var username = "EISAWESOME";
 
 
+    $scope.init = function(){
+      $scope.catchPhrase = "<clement-s/>";
+      $scope.avatarUrl = "https://github.com/EISAWESOME.png";
+    };
 
+    $scope.goBack = function(){
+    }
 
-  };
-
-  var tetris = new Konami(function(){
-    $scope.cardHide = true;
-    //Do the unlock animation
-    $timeout(function(){
-      $scope.tetrisShow = true;
+    var tetris = new Konami(function(){
+      //Do the unlock animation
+      //Hide maincard class
+      $scope.cardHide = true;
 
       $timeout(function(){
-        $('.game').blockrain({theme: "candy"});
-      },1001)
+        //Show tetris div
+        $scope.tetrisShow = true;
+
+        //Create the button to go back
+
+        $timeout(function(){
+          $('.game').blockrain({theme: "candy", playText: 'IT\'S A SECRET TO EVERYBODY',});
+        },1001)
 
 
-    }, 1001);
-    $scope.$apply()
+      }, 1001);
+      $scope.$apply()
+    })
 
-
-    //Hide maincard class
-
-    //Create the button to go back
-
-    //Create tetris div
-
-
-  })
-
-});
+  });
+})();
